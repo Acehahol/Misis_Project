3acde280c7bbdc7b60f04408dfc9d2f492dd6481
Acehahol <vlad.pehotin@mail.ru>
2023-03-05 13:44:41 +0300

    dublicate_app

--- project_cart/src/main/scala/main/AccountDbApp.scala
+++ project_cart/src/main/scala/main/AccountDbApp.scala
@@ -2,6 +2,7 @@ package main
 
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
+import com.typesafe.config.ConfigFactory
 import main.db.InitDb
 import main.repository.AccountRepositoryDb
 import main.route.AccountRoute
@@ -13,11 +14,11 @@ object AccountDbApp extends App {
     implicit val system: ActorSystem = ActorSystem("CartApp")
     implicit val ec: ExecutionContextExecutor = system.dispatcher
     implicit val db = Database.forConfig("database.postgres")
-
+    val port = ConfigFactory.load().getInt("port")
     new InitDb().prepare()
     val bank = new AccountRepositoryDb
     val itemroute = new AccountRoute(bank).route
 
-    Http().newServerAt("0.0.0.0", 8080).bind(itemroute)
+    Http().newServerAt("0.0.0.0", port).bind(itemroute)
 
 }
--- project_cart/src/main/scala/main/db/AccountDb.scala
+++ project_cart/src/main/scala/main/db/AccountDb.scala
@@ -7,14 +7,14 @@ import slick.lifted.Tag
 import java.util.UUID
 
 object AccountDb {
-  class ItemTable(tag: Tag) extends Table[Account](tag, "Accounts") {
-    val id = column[UUID]("id", O.PrimaryKey)
-    val firstname = column[String]("firstname")
-    val surname = column[String]("surname")
-    val cash = column[Int]("cash")
+    class ItemTable(tag: Tag) extends Table[Account](tag, "Accounts") {
+        val id = column[UUID]("id", O.PrimaryKey)
+        val firstname = column[String]("firstname")
+        val surname = column[String]("surname")
+        val cash = column[Int]("cash")
 
-    def * = (id, firstname, surname, cash) <> ((Account.apply _).tupled, Account.unapply)
-  }
+        def * = (id, firstname, surname, cash) <> ((Account.apply _).tupled, Account.unapply)
+    }
 
-  val itemTable = TableQuery[ItemTable]
+    val itemTable = TableQuery[ItemTable]
 }
